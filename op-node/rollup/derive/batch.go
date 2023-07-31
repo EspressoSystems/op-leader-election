af906d4b953cfc4915dc832a2146964de7febd73
Nathan F Yospe <nyospe@gmail.com>
2023-07-31 19:20:31 -0400

    Updating Batch header to V2

--- op-node/rollup/derive/batch.go
+++ op-node/rollup/derive/batch.go
@@ -18,11 +18,14 @@ import (
 // first byte is type followed by bytestring.
 //
 // BatchV1Type := 0
-// batchV1 := BatchV1Type ++ RLP([epoch, timestamp, transaction_list]
+// batchV1 := BatchV1Type ++ RLP([epoch, timestamp, transaction_list])
 //
 // An empty input is not a valid batch.
 //
 // Note: the type system is based on L1 typed transactions.
+//
+// BatchV2Type := 1
+// batchV2 := BatchV2Type ++ RLP([epoch, timestamp, fee_addr, transaction_list])
 
 // encodeBufferPool holds temporary encoder buffers for batch encoding
 var encodeBufferPool = sync.Pool{
@@ -31,6 +34,7 @@ var encodeBufferPool = sync.Pool{
 
 const (
 	BatchV1Type = iota
+	BatchV2Type = BatchV1Type + 1
 )
 
 type BatchV1 struct {
@@ -42,12 +46,21 @@ type BatchV1 struct {
 	Transactions []hexutil.Bytes
 }
 
-type BatchData struct {
+type BatchV2 struct {
+	PayToAddr common.Address
 	BatchV1
+}
+
+type BatchData struct {
+	BatchV2
 	// batches may contain additional data with new upgrades
 }
 
 func (b *BatchV1) Epoch() eth.BlockID {
+	return eth.BlockID{Hash: b.EpochHash, Number: uint64(b.EpochNum)}
+}
+
+func (b *BatchV2) Epoch() eth.BlockID {
 	return eth.BlockID{Hash: b.EpochHash, Number: uint64(b.EpochNum)}
 }
 
@@ -70,8 +83,8 @@ func (b *BatchData) MarshalBinary() ([]byte, error) {
 }
 
 func (b *BatchData) encodeTyped(buf *bytes.Buffer) error {
-	buf.WriteByte(BatchV1Type)
-	return rlp.Encode(buf, &b.BatchV1)
+	buf.WriteByte(BatchV2Type)
+	return rlp.Encode(buf, &b.BatchV2)
 }
 
 // DecodeRLP implements rlp.Decoder
@@ -101,6 +114,8 @@ func (b *BatchData) decodeTyped(data []byte) error {
 	switch data[0] {
 	case BatchV1Type:
 		return rlp.DecodeBytes(data[1:], &b.BatchV1)
+	case BatchV2Type:
+		return rlp.DecodeBytes(data[1:], &b.BatchV2)
 	default:
 		return fmt.Errorf("unrecognized batch type: %d", data[0])
 	}
