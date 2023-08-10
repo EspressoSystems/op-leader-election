fe16a110ce5d5c56743b6eb34760ff6d653cb5c0
Philippe Camacho <philippe@espressosys.com>
2023-08-10 06:21:48 -0400

    Initial implementation of reference contract.

--- packages/contracts-bedrock/src/L1/RoundRobinLeaderElection.sol
+++ packages/contracts-bedrock/src/L1/RoundRobinLeaderElection.sol
@@ -7,15 +7,69 @@ import "./LeaderElectionBatchInbox.sol";
 /// @notice Interface for implementing a leader election scheme
 
 contract RoundRobinLeaderElection is ILeaderElectionBatchInbox {
+    address immutable owner;
+    uint256 max_number_participants;
+    uint256 index_last_inserted_participant;
+    mapping(uint256 => address) public participants;
+    mapping(address => bool) public is_participant;
+    // TODO No need to be public, just for testing purposes. Do this more cleanly
+    uint256 public creation_block_number;
+    uint8 constant HORIZON = 10; // Number of leader slots that can be checked in advance
 
-    constructor () public {}
-
-    function isCurrentLeader(address leaderId, uint256 blockNumber) external view returns (bool){
-        return false;
+    // TODO gas optimization
+    function _uint256FromBytesLittleEndian(uint8[HORIZON] memory input) private pure returns (uint256) {
+        uint256 r = 0;
+        for (uint256 i = 0; i < input.length; i++) {
+            r += 2 ** (8 * i) * input[i];
+        }
+        return r;
     }
 
+    constructor(uint256 n) {
+        owner = msg.sender;
+        max_number_participants = n;
+        creation_block_number = block.number;
+    }
+
+    function addParticipant(address addr) public {
+        // TODO write this more cleanly
+        if (msg.sender != owner) {
+            revert("Only the creator of this contract can call this function.");
+        }
+        if (index_last_inserted_participant == max_number_participants) {
+            revert("List of participants is full.");
+        }
+
+        participants[index_last_inserted_participant] = addr;
+        is_participant[addr] = true;
+        index_last_inserted_participant++;
+    }
+
+    function isCurrentLeader(address leaderId, uint256 blockNumber) external view returns (bool) {
+        if (blockNumber < creation_block_number) {
+            // if the block number is "too old" then no one is leader
+            return false;
+        } else {
+            uint256 index_leader = (blockNumber - creation_block_number) % max_number_participants;
+            return participants[index_leader] == leaderId;
+        }
+    }
 
     function nextBlocksAsLeader() external view returns (LeaderStatusFlags, uint256, uint256, uint8) {
-        return (LeaderStatusFlags.Scheduled, 1,1,1);
+        if (!this.is_participant(msg.sender)) {
+            return (LeaderStatusFlags.Invalid, 0, 0, 0);
+        } else {
+            // Build the bitmap
+            uint8[HORIZON] memory leaderSlots;
+            for (uint256 i = 0; i < HORIZON; i++) {
+                if (this.isCurrentLeader(msg.sender, block.number)) {
+                    leaderSlots[i] = 1;
+                } else {
+                    leaderSlots[i] = 0;
+                }
+            }
+            uint256 bitmapAsInt = _uint256FromBytesLittleEndian(leaderSlots);
+            return (LeaderStatusFlags.Scheduled, block.number, bitmapAsInt, HORIZON);
+        }
     }
 }
--- packages/contracts-bedrock/test/RoundRobinLeaderElection.t.sol
+++ packages/contracts-bedrock/test/RoundRobinLeaderElection.t.sol
@@ -1,19 +1,48 @@
 pragma solidity 0.8.15;
 
-import { Test } from "forge-std/Test.sol";
+import {Test} from "forge-std/Test.sol";
 
 import "../src/L1/RoundRobinLeaderElection.sol";
 
 contract RoundRobinLeaderElectionTest is Test {
-
     RoundRobinLeaderElection leaderContract;
+    uint256 public constant N_PARTICIPANTS = 5;
+    uint256 DEPLOYMENT_BLOCK_NUMBER = 100;
 
     function setUp() public {
-        leaderContract = new RoundRobinLeaderElection();
+        vm.roll(DEPLOYMENT_BLOCK_NUMBER);
+        leaderContract = new RoundRobinLeaderElection(N_PARTICIPANTS);
+
+        // Poblate the list of participants who are allowed to vote
+        for (uint256 i = 1; i <= N_PARTICIPANTS; i++) {
+            address addr = vm.addr(i);
+            leaderContract.addParticipant(addr);
+        }
     }
 
-    function testIsLeader() external {
-        assertTrue(leaderContract.isCurrentLeader(0x0000000000000000000000000000000000000000,0));
+    function test_rrElection_ListParticipants() external {
+        for (uint256 i = 0; i < N_PARTICIPANTS; i++) {
+            assertEq(vm.addr(i + 1), leaderContract.participants(i));
+        }
     }
 
+    function test_rrElection_ListParticipantsIsFull() external {
+        vm.expectRevert("List of participants is full.");
+        leaderContract.addParticipant(vm.addr(52));
+    }
+
+    function test_rrElection_IsLeader() external {
+        assertEq(block.number, DEPLOYMENT_BLOCK_NUMBER);
+        assertEq(leaderContract.creation_block_number(), DEPLOYMENT_BLOCK_NUMBER);
+
+        // No one is a leader for an old block
+        for (uint256 i = 1; i <= N_PARTICIPANTS; i++) {
+            address participant = leaderContract.participants(i - 1);
+            assertFalse(leaderContract.isCurrentLeader(participant, 1));
+        }
+
+        // When the contract is deployed the leader is the first participant
+        assertTrue(leaderContract.isCurrentLeader(vm.addr(1), DEPLOYMENT_BLOCK_NUMBER));
+        assertTrue(leaderContract.isCurrentLeader(vm.addr(2), DEPLOYMENT_BLOCK_NUMBER + 1));
+    }
 }
