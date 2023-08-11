1b2fab01511a9223f80ec26ec0a70a6bd6bd9696
Mathis <sveitser@gmail.com>
2023-08-11 16:34:29 +0200

    Update op-node/rollup/derive/system_config.go

--- op-node/rollup/derive/system_config.go
+++ op-node/rollup/derive/system_config.go
@@ -87,7 +87,7 @@ func ProcessSystemConfigUpdateLogEvent(destSysCfg *eth.SystemConfig, ev *types.L
 		if err != nil {
 			return NewCriticalError(errors.New("could not read batcher hash"))
 		}
-		if version >= 1 {
+		if version > 1 {
 			return NewCriticalError(errors.New("unsupported batcher hash version"))
 		}
 		// Update the batcher address if the version byte is 0 (version 1)
